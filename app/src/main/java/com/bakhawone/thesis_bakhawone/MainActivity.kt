5cfb1d4744bf444c69c6c1033c68f2abb370e6c3
Karen Angela Realubit <142026862+Angela1104@users.noreply.github.com>
2025-10-03 00:09:11 +0800

    first commit

---
+++ app/src/main/java/com/bakhawone/thesis_bakhawone/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.bakhawone.thesis_bakhawone
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        val btnStartScan: Button = findViewById(R.id.btnStartScan)
+
+        btnStartScan.setOnClickListener {
+            val intent = Intent(this, CamActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
